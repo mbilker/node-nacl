39b0e17c7df64331237f5705a853b5a1c7d9db07
Brian Warner <warner@lothar.com>
2012-12-16 18:43:16 -0800

    use modified nacl-20110221 (use ed25519 instead of edwards25519sha512batch)

---
+++ subnacl/include/crypto_int32.h
@@ -0,0 +1,5 @@
+#ifndef crypto_int32_H
+#define crypto_int32_H
+
+typedef unsigned int crypto_int32;
+#endif
---
+++ subnacl/include/crypto_sign_ed25519.h
@@ -0,0 +1,29 @@
+#ifndef crypto_sign_ed25519_H
+#define crypto_sign_ed25519_H
+
+#define crypto_sign_ed25519_SECRETKEYBYTES 64
+#define crypto_sign_ed25519_PUBLICKEYBYTES 32
+#define crypto_sign_ed25519_BYTES 64
+
+
+#ifdef __cplusplus
+
+extern std::string crypto_sign_ed25519(const std::string &,const std::string &);
+extern std::string crypto_sign_ed25519_open(const std::string &,const std::string &);
+extern std::string crypto_sign_ed25519_keypair(std::string *);
+
+extern "C" {
+#endif
+
+extern int crypto_sign_ed25519(unsigned char *,unsigned long long *,const unsigned char *,unsigned long long,const unsigned char *);
+extern int crypto_sign_ed25519_open(unsigned char *,unsigned long long *,const unsigned char *,unsigned long long,const unsigned char *);
+extern int crypto_sign_ed25519_keypair(unsigned char *,unsigned char *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#define crypto_sign_ed25519_IMPLEMENTATION "crypto_sign/ed25519/ref"
+#define crypto_sign_ed25519_VERSION "-"
+
+#endif

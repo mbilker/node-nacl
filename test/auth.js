5ae50f76c834bbaa29b316ff01b39f091b1f6eba
Brian Warner <warner@lothar.com>
2012-12-24 17:31:24 -0800

    improve auth tests

--- test/auth.js
+++ test/auth.js
@@ -17,23 +17,34 @@ function padKey(hex) {
     return key;
 }
 
-test("auth", function(t) {
+test("auth RFC 4321", function(t) {
     // test vectors from RFC 4321. However, nacl_auth_sha512256 (meaning
     // HMAC-SHA512 with the output truncated down to 256 bits) only
     // implements key.length=32, so the HMAC-SHA512 is always zero-padded out
     // to 128 bytes. Thus the domain of nacl_auth_sha512256 is does not
     // include any test vector with a key longer than 32 bytes, so we skip
     // those tests.
-    t.equivalent(n.auth(Buffer("Hi There"), 
+
+    t.equivalent(n.auth(Buffer("Hi There"),
                         padKey("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b")),
                  fromHex("87aa7cdea5ef619d4ff0b4241a1d6cb0"+
                          "2379f4e2ce4ec2787ad0b30545e17cde"),
                 "Test Case 1");
+    n.auth_verify(fromHex("87aa7cdea5ef619d4ff0b4241a1d6cb0"+
+                          "2379f4e2ce4ec2787ad0b30545e17cde"),
+                  Buffer("Hi There"),
+                  padKey("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b"));
+
     t.equivalent(n.auth(Buffer("what do ya want for nothing?"),
                         padKey("4a656665")),
                  fromHex("164b7a7bfcf819e2e395fbe73b56e0a3"+
                          "87bd64222e831fd610270cd7ea250554"),
                  "Test Case 2");
+    n.auth_verify(fromHex("164b7a7bfcf819e2e395fbe73b56e0a3"+
+                          "87bd64222e831fd610270cd7ea250554"),
+                  Buffer("what do ya want for nothing?"),
+                  padKey("4a656665"));
+
     t.equivalent(n.auth(fromHex("dddddddddddddddddddddddddddddddd"+
                                 "dddddddddddddddddddddddddddddddd"+
                                 "dddddddddddddddddddddddddddddddd"+
@@ -42,6 +53,14 @@ test("auth", function(t) {
                  fromHex("fa73b0089d56a284efb0f0756c890be9"+
                          "b1b5dbdd8ee81a3655f83e33b2279d39"),
                  "Test Case 3");
+    n.auth_verify(fromHex("fa73b0089d56a284efb0f0756c890be9"+
+                          "b1b5dbdd8ee81a3655f83e33b2279d39"),
+                  fromHex("dddddddddddddddddddddddddddddddd"+
+                          "dddddddddddddddddddddddddddddddd"+
+                          "dddddddddddddddddddddddddddddddd"+
+                          "dddd"),
+                  padKey("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+
     t.equivalent(n.auth(fromHex("cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd"+
                                 "cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd"+
                                 "cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd"+
@@ -51,6 +70,15 @@ test("auth", function(t) {
                  fromHex("b0ba465637458c6990e5a8c5f61d4af7"+
                          "e576d97ff94b872de76f8050361ee3db"),
                  "Test Case 4");
+    n.auth_verify(fromHex("b0ba465637458c6990e5a8c5f61d4af7"+
+                          "e576d97ff94b872de76f8050361ee3db"),
+                  fromHex("cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd"+
+                          "cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd"+
+                          "cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd"+
+                          "cdcd"),
+                  padKey("0102030405060708090a0b0c0d0e0f10"+
+                         "111213141516171819"));
+
     t.equivalent(n.auth(Buffer("Test With Truncation"),
                         padKey("0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c"+
                                "0c0c0c0c")).slice(0, 16),
@@ -84,5 +112,35 @@ test("auth", function(t) {
                          "debd71f8867289865df5a32d20cdc944"),
                  "Test Case 7");
      */
+    t.end();
+});
+
+function shouldFail(t, a, m, k) {
+    try {
+        n.auth_verify(a, m, k);
+        t.fail("forgery");
+    } catch (e) {
+        t.equal(e.message, "invalid authenticator");
+    }
+}
+
+test("auth basic", function(t) {
+    var key = padKey("00010203");
+    var msg = Buffer("Hello World");
+    var a = n.auth(msg, key);
+    n.auth_verify(a, msg, key);
+    shouldFail(t, a, Buffer.concat([msg, Buffer("extra")]), key);
+    var not_a = Buffer(a);
+    not_a.writeUInt8((1+a.readUInt8(0))%256, 0);
+    shouldFail(t, not_a, msg, key);
+
+    var not_msg = Buffer(msg);
+    not_msg.writeUInt8((1+msg.readUInt8(0))%256, 0);
+    shouldFail(t, a, not_msg, key);
+
+    var not_key = Buffer(key);
+    not_key.writeUInt8((1+key.readUInt8(0))%256, 0);
+    shouldFail(t, a, msg, not_key);
+
     t.end();
 });

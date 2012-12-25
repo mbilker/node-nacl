1628e8563bb5b3995fd88eb553631b5618cedcd3
Brian Warner <warner@lothar.com>
2012-12-24 22:45:20 -0800

    tolerate more bad arguments

--- node_nacl.cc
+++ node_nacl.cc
@@ -51,7 +51,12 @@ static Handle<Value> node_crypto_stream_xor (const Arguments&);
 #define EXTRACT_BUFFER_ARG(arg, var, msg) \
   if (!Buffer::HasInstance(arg)) \
     return ThrowException(Exception::TypeError(String::New(msg " must be a Buffer"))); \
-  string var = buf_to_str(arg->ToObject()); \
+  string var = buf_to_str(arg->ToObject());
+
+#define EXTRACT_INTEGER_ARG(arg, var, msg) \
+  if (!arg->IsUint32())                      \
+    return ThrowException(Exception::TypeError(String::New(msg " must be an Integer"))); \
+  size_t var = arg->Uint32Value();
 
 static string buf_to_str (Handle<Object> b) {
   return string(Buffer::Data(b), Buffer::Length(b));
@@ -68,10 +73,11 @@ static Buffer* str_to_buf (string s) {
 
 static Handle<Value> node_crypto_box (const Arguments& args) {
   HandleScope scope;
-  string m = buf_to_str(args[0]->ToObject());
-  string n = buf_to_str(args[1]->ToObject());
-  string pk = buf_to_str(args[2]->ToObject());
-  string sk = buf_to_str(args[3]->ToObject());
+  BAIL_IF_NOT_N_ARGS(4, "Args: message, nonce, pubkey, privkey");
+  EXTRACT_BUFFER_ARG(args[0], m, "arg[0] 'message'");
+  EXTRACT_BUFFER_ARG(args[1], n, "arg[1] 'nonce'");
+  EXTRACT_BUFFER_ARG(args[2], pk, "arg[2] 'pubkey'");
+  EXTRACT_BUFFER_ARG(args[3], sk, "arg[3] 'privkey'");
   try {
     string c = crypto_box(m,n,pk,sk);
     return scope.Close(str_to_buf(c)->handle_);
@@ -82,10 +88,11 @@ static Handle<Value> node_crypto_box (const Arguments& args) {
 
 static Handle<Value> node_crypto_box_open (const Arguments& args) {
   HandleScope scope;
-  string c = buf_to_str(args[0]->ToObject());
-  string n = buf_to_str(args[1]->ToObject());
-  string pk = buf_to_str(args[2]->ToObject());
-  string sk = buf_to_str(args[3]->ToObject());
+  BAIL_IF_NOT_N_ARGS(4, "Args: ciphertext, nonce, pubkey, privkey");
+  EXTRACT_BUFFER_ARG(args[0], c, "arg[0] 'ciphertext'");
+  EXTRACT_BUFFER_ARG(args[1], n, "arg[1] 'nonce'");
+  EXTRACT_BUFFER_ARG(args[2], pk, "arg[2] 'pubkey'");
+  EXTRACT_BUFFER_ARG(args[3], sk, "arg[3] 'privkey'");
   try {
     string m = crypto_box_open(c,n,pk,sk);
     return scope.Close(str_to_buf(m)->handle_);
@@ -108,8 +115,9 @@ static Handle<Value> node_crypto_box_keypair (const Arguments& args) {
 
 static Handle<Value> node_crypto_sign (const Arguments& args) {
   HandleScope scope;
-  string m = buf_to_str(args[0]->ToObject());
-  string sk = buf_to_str(args[1]->ToObject());
+  BAIL_IF_NOT_N_ARGS(2, "Args: message, signingkey");
+  EXTRACT_BUFFER_ARG(args[0], m, "arg[0] 'message'");
+  EXTRACT_BUFFER_ARG(args[1], sk, "arg[1] 'signingkey'");
   try {
     string sm = crypto_sign(m,sk);
     return scope.Close(str_to_buf(sm)->handle_);
@@ -120,8 +128,9 @@ static Handle<Value> node_crypto_sign (const Arguments& args) {
 
 static Handle<Value> node_crypto_sign_open (const Arguments& args) {
   HandleScope scope;
-  string sm = buf_to_str(args[0]->ToObject());
-  string pk = buf_to_str(args[1]->ToObject());
+  BAIL_IF_NOT_N_ARGS(2, "Args: signedmessage, verifyingkey");
+  EXTRACT_BUFFER_ARG(args[0], sm, "arg[0] 'signedmessage'");
+  EXTRACT_BUFFER_ARG(args[1], pk, "arg[1] 'verifyingkey'");
   try {
     string m = crypto_sign_open(sm,pk);
     return scope.Close(str_to_buf(m)->handle_);
@@ -143,7 +152,8 @@ static Handle<Value> node_crypto_sign_keypair (const Arguments& args) {
 
 static Handle<Value> node_crypto_sign_publickey (const Arguments& args) {
   HandleScope scope;
-  string seed = buf_to_str(args[0]->ToObject());
+  BAIL_IF_NOT_N_ARGS(1, "Args: seed");
+  EXTRACT_BUFFER_ARG(args[0], seed, "arg[0] 'seed'");
   string sk;
   Buffer* pk_buf = str_to_buf(crypto_sign_publickey(seed, &sk));
   Buffer* sk_buf = str_to_buf(sk);
@@ -155,9 +165,10 @@ static Handle<Value> node_crypto_sign_publickey (const Arguments& args) {
 
 static Handle<Value> node_crypto_secretbox (const Arguments& args) {
   HandleScope scope;
-  string m = buf_to_str(args[0]->ToObject());
-  string n = buf_to_str(args[1]->ToObject());
-  string k = buf_to_str(args[2]->ToObject());
+  BAIL_IF_NOT_N_ARGS(3, "Args: message, nonce, key");
+  EXTRACT_BUFFER_ARG(args[0], m, "arg[0] 'message'");
+  EXTRACT_BUFFER_ARG(args[1], n, "arg[1] 'nonce'");
+  EXTRACT_BUFFER_ARG(args[2], k, "arg[2] 'key'");
   try {
     string c = crypto_secretbox(m,n,k);
     return scope.Close(str_to_buf(c)->handle_);
@@ -168,9 +179,10 @@ static Handle<Value> node_crypto_secretbox (const Arguments& args) {
 
 static Handle<Value> node_crypto_secretbox_open (const Arguments& args) {
   HandleScope scope;
-  string c = buf_to_str(args[0]->ToObject());
-  string n = buf_to_str(args[1]->ToObject());
-  string k = buf_to_str(args[2]->ToObject());
+  BAIL_IF_NOT_N_ARGS(3, "Args: ciphertext, nonce, key");
+  EXTRACT_BUFFER_ARG(args[0], c, "arg[0] 'ciphertext'");
+  EXTRACT_BUFFER_ARG(args[1], n, "arg[1] 'nonce'");
+  EXTRACT_BUFFER_ARG(args[2], k, "arg[2] 'key'");
   try {
     string m = crypto_secretbox_open(c,n,k);
     return scope.Close(str_to_buf(m)->handle_);
@@ -181,8 +193,9 @@ static Handle<Value> node_crypto_secretbox_open (const Arguments& args) {
 
 static Handle<Value> node_crypto_onetimeauth (const Arguments& args) {
   HandleScope scope;
-  string m = buf_to_str(args[0]->ToObject());
-  string k = buf_to_str(args[1]->ToObject());
+  BAIL_IF_NOT_N_ARGS(2, "Args: message, key");
+  EXTRACT_BUFFER_ARG(args[0], m, "arg[0] 'message'");
+  EXTRACT_BUFFER_ARG(args[1], k, "arg[1] 'key'");
   try {
     string a = crypto_onetimeauth(m,k);
     return scope.Close(str_to_buf(a)->handle_);
@@ -193,9 +206,10 @@ static Handle<Value> node_crypto_onetimeauth (const Arguments& args) {
 
 static Handle<Value> node_crypto_onetimeauth_verify (const Arguments& args) {
   HandleScope scope;
-  string a = buf_to_str(args[0]->ToObject());
-  string m = buf_to_str(args[1]->ToObject());
-  string k = buf_to_str(args[2]->ToObject());
+  BAIL_IF_NOT_N_ARGS(3, "Args: authenticator, message, key");
+  EXTRACT_BUFFER_ARG(args[0], a, "arg[0] 'authenticator'");
+  EXTRACT_BUFFER_ARG(args[1], m, "arg[1] 'message'");
+  EXTRACT_BUFFER_ARG(args[2], k, "arg[2] 'key'");
   try {
     crypto_onetimeauth_verify(a,m,k);
     return scope.Close(Null());
@@ -233,9 +247,10 @@ static Handle<Value> node_crypto_auth_verify (const Arguments& args) {
 
 static Handle<Value> node_crypto_stream (const Arguments& args) {
   HandleScope scope;
-  size_t clen = args[0]->IntegerValue();
-  string n = buf_to_str(args[1]->ToObject());
-  string k = buf_to_str(args[2]->ToObject());
+  BAIL_IF_NOT_N_ARGS(3, "Args: length, nonce, key");
+  EXTRACT_INTEGER_ARG(args[0], clen, "arg[0] 'length'");
+  EXTRACT_BUFFER_ARG(args[1], n, "arg[1] 'nonce'");
+  EXTRACT_BUFFER_ARG(args[2], k, "arg[2] 'key'");
   try {
     string c = crypto_stream(clen,n,k);
     return scope.Close(str_to_buf(c)->handle_);
@@ -246,9 +261,10 @@ static Handle<Value> node_crypto_stream (const Arguments& args) {
 
 static Handle<Value> node_crypto_stream_xor (const Arguments& args) {
   HandleScope scope;
-  string m = buf_to_str(args[0]->ToObject());
-  string n = buf_to_str(args[1]->ToObject());
-  string k = buf_to_str(args[2]->ToObject());
+  BAIL_IF_NOT_N_ARGS(3, "Args: message, nonce, key");
+  EXTRACT_BUFFER_ARG(args[0], m, "arg[0] 'message'");
+  EXTRACT_BUFFER_ARG(args[1], n, "arg[1] 'nonce'");
+  EXTRACT_BUFFER_ARG(args[2], k, "arg[2] 'key'");
   try {
     string c = crypto_stream_xor(m,n,k);
     return scope.Close(str_to_buf(c)->handle_);
